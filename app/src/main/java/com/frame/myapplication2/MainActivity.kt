15dbdefb16500477967f91e09d3c1942cf48fe82
EastLoong <517367549@qq.com>
2019-10-08 23:12:32 +0800

    第二次测试提交

--- app/src/main/java/com/frame/myapplication2/MainActivity.kt
+++ app/src/main/java/com/frame/myapplication2/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     //测试提交
+    //第二次测试提交
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
